0b5626e37d6d346b6215d11866678888ee0881b9
davitonP <velascod@uabc.edu.mx>
2024-03-07 16:04:54 -0800

    primera version funcional

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ import { readFileSync } from 'fs';
 
 const data = readFileSync('./test-data/test.csv', 'utf8');
 let dash = new DashboardCsv(data);
+dash.orderDataByDate();
+dash.convertToNumber();
+
+dash.showHeaders();
+dash.showDeltaValues();
 dash.showData();
 
 export default DashboardCsv;
--- src/dashboard_csv.js
+++ src/dashboard_csv.js
@@ -1,8 +1,49 @@
 export default class DashboardCsv {
+
+  /*
+  data: [
+    ['date', 'impressions', 'clicks', 'revenue'],
+    ['2019-01-01', '100', '10', '1.23'],
+    ['2019-01-02', '200', '20', '2.23'],
+    ['2019-01-03', '300', '30', '3.23']
+  ]
+  */
   constructor(data) {
     this.data = data;
     this.separateData();
     this.separateByComma();
+    this.headers();
+    this.removeEmptyRows();
+    this.createDeltaValues();
+    // this.getDeltaValues();
+  }
+
+  headers() {
+    this.header = this.data.shift();
+  }
+  getHeaders() {
+    return this.header;
+  }
+  showHeaders() {
+    console.log(this.header);
+  }
+
+  createDeltaValues() {
+    this.delta = [];
+    let length = this.data.length;
+
+    let last = this.data[length - 2];
+    let current = this.data[length - 1];
+
+    // console.log(last);
+    // console.log(current);
+
+    for (let i = 0; i < last.length; i++) {
+      this.delta.push(current[i] - last[i]);
+    }
+  }
+  showDeltaValues() {
+    console.log(this.delta);
   }
 
   getNRows() {
@@ -20,8 +61,33 @@ export default class DashboardCsv {
     }
   }
 
-  
   showData() {
     console.log(this.data);
+  }
+
+  orderDataByDate() {
+    this.data.sort((a, b) => {
+      return new Date(a[0]) - new Date(b[0]);
+    });
+  }
+
+  convertToNumber() {
+    for (let i = 0; i < this.data.length; i++) {
+      for (let j = 1; j < this.data[i].length; j++) {
+        try {
+          this.data[i][j] = this.data[i][j].replace(/,/g, '');
+          this.data[i][j] = Number(this.data[i][j]);
+        } catch (e) {
+          console.log(e);
+        }
+      }
+    }
+  }
+
+
+  removeEmptyRows() {
+    this.data = this.data.filter(row => {
+      return row.length > 2;
+    });
   }
 }
